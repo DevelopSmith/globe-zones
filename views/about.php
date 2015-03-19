b3551cef63333e0f13658fc833ca10c4d5be2570
DevelopSmith <designsuniverse@gmail.com>
2015-03-20 01:21:44 +0200

    Little SQL.

--- views/about.php
+++ views/about.php
@@ -6,15 +6,15 @@ $db = pg_connect("host=ec2-184-73-221-47.compute-1.amazonaws.com port=5432 dbnam
 $query_drop = "DROP TABLE films";
 pg_exec($db, $query_drop);
 
-$sql = "CREATE TABLE articles (
+/*$sql = "CREATE TABLE articles (
     id          integer,
     title       varchar(40),
     context     varchar(100),
     date_prod   date
 )";
-pg_exec($db, $sql);
+pg_exec($db, $sql);*/
 
-$ins = INSERT INTO articles(id, title, context) VALUES('1', 'Introduction', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+$ins = INSERT INTO articles(id, title, context) VALUES('1', 'Introduction', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
 
 pg_exec($db, $ins);
 ?>
