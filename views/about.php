be0efabfba18c9f0e93b1820d3f60122a6a8467a
DevelopSmith <designsuniverse@gmail.com>
2015-03-20 01:06:21 +0200

    Add another table.

--- views/about.php
+++ views/about.php
@@ -1,14 +1,13 @@
 <?php
-echo 'We are very awesome!';
+echo 'We are very awesome!!!';
 
 $db = pg_connect("host=ec2-184-73-221-47.compute-1.amazonaws.com port=5432 dbname=d4gggc013bg6aa user=ymvxejayjiolvt password=spFaxG9g21JkZSWv68UYft-Q_W");
 
-$sql = "CREATE TABLE films (
-    code        char(5),
+$sql = "CREATE TABLE articles (
+    id          integer,
     title       varchar(40),
-    did         integer,
-    date_prod   date,
-    kind        varchar(10)
+    context     varchar(100),
+    date_prod   date
 )";
 pg_exec($db, $sql)
 ?>
