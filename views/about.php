9af43b2f0cc9d85f1fd4a1ebacf1899d7ea3313b
DevelopSmith <designsuniverse@gmail.com>
2015-03-20 01:02:43 +0200

    Made another little change in About.php.

--- views/about.php
+++ views/about.php
@@ -1,17 +1,14 @@
 <?php
-echo 'We are really awesome!';
+echo 'We are very awesome!';
 
-$dbconn4 = pg_connect("host=ec2-184-73-221-47.compute-1.amazonaws.com port=5432 dbname=d4gggc013bg6aa user=ymvxejayjiolvt password=spFaxG9g21JkZSWv68UYft-Q_W");
+$db = pg_connect("host=ec2-184-73-221-47.compute-1.amazonaws.com port=5432 dbname=d4gggc013bg6aa user=ymvxejayjiolvt password=spFaxG9g21JkZSWv68UYft-Q_W");
 
-
-/*CREATE TABLE films (
+$sql = "CREATE TABLE films (
     code        char(5),
     title       varchar(40),
     did         integer,
     date_prod   date,
-    kind        varchar(10),
-    len         interval hour to minute,
-    CONSTRAINT production UNIQUE(date_prod)
-);*/
-
+    kind        varchar(10)
+)";
+pg_exec($db, $sql)
 ?>
