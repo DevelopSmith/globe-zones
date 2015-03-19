5ced19bf7580a0f7180f4fe0a641bf2bfc70687d
DevelopSmith <designsuniverse@gmail.com>
2015-03-20 01:24:40 +0200

    Little SQL.

--- views/about.php
+++ views/about.php
@@ -3,8 +3,8 @@ echo 'We are very awesome!!! Yes I am sure';
 
 $db = pg_connect("host=ec2-184-73-221-47.compute-1.amazonaws.com port=5432 dbname=d4gggc013bg6aa user=ymvxejayjiolvt password=spFaxG9g21JkZSWv68UYft-Q_W");
 
-$query_drop = "DROP TABLE films";
-pg_exec($db, $query_drop);
+/*$query_drop = "DROP TABLE films";
+pg_exec($db, $query_drop);*/
 
 /*$sql = "CREATE TABLE articles (
     id          integer,
