41f6b8163ae1c72cb93bbb6d8918f050a91ef0e7
DevelopSmith <designsuniverse@gmail.com>
2015-03-20 01:16:49 +0200

    Some SQL work.

--- views/about.php
+++ views/about.php
@@ -1,7 +1,10 @@
 <?php
-echo 'We are very awesome!!!';
+echo 'We are very awesome!!! Yes I am sure';
 
 $db = pg_connect("host=ec2-184-73-221-47.compute-1.amazonaws.com port=5432 dbname=d4gggc013bg6aa user=ymvxejayjiolvt password=spFaxG9g21JkZSWv68UYft-Q_W");
+
+$query_drop = "DROP TABLE films";
+pg_exec($db, $query_drop);
 
 $sql = "CREATE TABLE articles (
     id          integer,
@@ -9,5 +12,9 @@ $sql = "CREATE TABLE articles (
     context     varchar(100),
     date_prod   date
 )";
-pg_exec($db, $sql)
+pg_exec($db, $sql);
+
+$ins = INSERT INTO articles(id, title, context) VALUES('1', 'Introduction', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+
+pg_exec($db, $ins);
 ?>
