8f330a982124c333d724b032852e20da7b22235d
DevelopSmith <designsuniverse@gmail.com>
2015-03-20 05:48:11 +0200

    ten

--- views/about.php
+++ views/about.php
@@ -20,10 +20,7 @@ $sql = "CREATE TABLE articles (
 )";
 pg_exec($db, $sql);*/
 
-/*$ins = "INSERT INTO articles(article_id, article_title, article_text, article_author) VALUES(1, 'Introduction', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'DevelopSmith')";
-pg_exec($db, $ins);*/
-
-$ins = "INSERT INTO articles(article_id, article_title, article_text, article_author) VALUES(2, 'About Us', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Heba Korany')";
+$ins = "INSERT INTO articles(article_id, article_title, article_text, article_author) VALUES(2, 'Introduction', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatm.', 'Heba Korany')";
 pg_exec($db, $ins);
 
 $sel = "SELECT * FROM articles ORDER BY article_id";
