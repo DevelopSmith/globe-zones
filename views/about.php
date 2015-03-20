0cf3394e7a0e44d5d4c58eff10c537f05ab2af38
DevelopSmith <designsuniverse@gmail.com>
2015-03-20 04:58:24 +0200

    Disabled

--- views/about.php
+++ views/about.php
@@ -1,11 +1,11 @@
 <?php
 $db = pg_connect("host=ec2-184-73-221-47.compute-1.amazonaws.com port=5432 dbname=d4gggc013bg6aa user=ymvxejayjiolvt password=spFaxG9g21JkZSWv68UYft-Q_W");
 
-$query_drop = "DROP TABLE articles";
-pg_exec($db, $query_drop);
+/*$query_drop = "DROP TABLE articles";
+pg_exec($db, $query_drop);*/
 
 $sql = "CREATE TABLE articles (
-	article_id		intger(100) AUTO_INCREMENT PRIMARY KEY,
+	article_id		intger(100),
 	article_title	varchar(400),
 	article_text 	text,
 	article_author	varchar(100),
