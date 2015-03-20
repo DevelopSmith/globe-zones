0b7c768df41c8bf536c6072715c85631ab28f9ac
DevelopSmith <designsuniverse@gmail.com>
2015-03-20 05:46:03 +0200

    nine

--- views/about.php
+++ views/about.php
@@ -24,7 +24,7 @@ pg_exec($db, $sql);*/
 pg_exec($db, $ins);*/
 
 $ins = "INSERT INTO articles(article_id, article_title, article_text, article_author) VALUES(2, 'About Us', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Heba Korany')";
-$ads = pg_exec($db, $ins);
+pg_exec($db, $ins);
 
 $sel = "SELECT * FROM articles ORDER BY article_id";
 $selt = pg_exec($db, $sel);
