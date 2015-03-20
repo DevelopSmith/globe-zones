cd90ce66ec438b10292865801ea5b5e551ee0b02
Mohamed Hasan <designsuniverse@gmail.com>
2015-03-20 02:53:18 +0200

    Update about.php

--- views/about.php
+++ views/about.php
@@ -3,7 +3,7 @@ echo 'We are very awesome!!! Yes I am sure';
 
 $db = pg_connect("host=ec2-184-73-221-47.compute-1.amazonaws.com port=5432 dbname=d4gggc013bg6aa user=ymvxejayjiolvt password=spFaxG9g21JkZSWv68UYft-Q_W");
 
-$query_drop = "DROP TABLE films";
+/*$query_drop = "DROP TABLE films";
 pg_exec($db, $query_drop);
 
 $sql = "CREATE TABLE articles (
@@ -16,5 +16,10 @@ pg_exec($db, $sql);
 
 $ins = "INSERT INTO articles(id, title, context) VALUES('1', 'Introduction', 'Lorem ipsum dolor sit amet, consectetur adipisici.')";
 
-pg_exec($db, $ins);
+pg_exec($db, $ins);*/
+
+$sel = "SELECT * FROM article ORDER BY id";
+$sel = pg_exec($db, $sel);
+
+print_r($sel);
 ?>
